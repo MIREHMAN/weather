2abbce8a4f574344c28968bd374a823e48a97c26
MIREHMAN <Inammalik73@gmail.com>
2024-10-16 21:27:46 +0500

    Update 1.0

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,28 +1,29 @@
 import "../components/Weather.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import clearIcon from '../assets/clear.png';
 
 function Weather() {
   useEffect(()=>{
-    Search('London');
+    Search('Islamabad');
   },[])
-
+  const inputRef=useRef();
   const [temp, setTemp] = useState("35");
   const [city, setCity] = useState("Rawalpindi");
   const [humidity, setHumidity] = useState("35");
   const [wind, setWind] = useState("1.4");
-  const [input, setInput] = useState("");
+  
 
   const api_key=process.env.REACT_APP_API_KEY;
 
-  const Search = async (city1) => {
+  const Search = async (input) => {
 
     try {
-      const url= `https://api.openweathermap.org/data/2.5/weather?q=${city1}&appid=${api_key}`;
+      const url= `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&appid=${api_key}`;
 
       const response= await fetch(url);
       const data= await response.json();
       setCity(data.name);
-      setTemp(data.main.temp);
+      setTemp(Math.floor(data.main.temp));
       setWind(data.wind.speed);
       setHumidity(data.main.humidity)
 
@@ -44,23 +45,30 @@ function Weather() {
             <input
               type="text"
               placeholder="Enter city name"
-              value={input}
-              onChange={(e) => {
-                setInput(e.target.value);
-              }}
+              ref={inputRef}
+
+              
+              
             />
-            <input type="button" value={"Se"}  />
+            <input type="button" value={"Se"} onClick={()=>Search(inputRef.current.value)} />
           </form>
         </div>
       </div>
       <div className="body-box">
         <div className="weather-image">
-          <img src="/" alt="weather" />
+          <img src={clearIcon} alt="weather" />
         </div>
-        <div className="temp-value">{`${temp} C`}</div>
+        <div className="temp-value">{temp}°C</div>
         <div className="city-name">{city}</div>
       </div>
       <div className="footer-box">
+      <div className="wind-speed-box">
+          <img src="/" alt="wind speed" />
+          <div className="windspeed">
+            <div className="windspeed-value">{wind} km/hr</div>
+            <div className="windspeed-text">Wind Speed</div>
+          </div>
+        </div>
         <div className="humidity-box">
           <img src="/" alt="humidity" />
           <div className="humidity">
@@ -68,13 +76,7 @@ function Weather() {
             <div className="humidity-text">Humidity</div>
           </div>
         </div>
-        <div className="wind-speed-box">
-          <img src="/" alt="wind speed" />
-          <div className="windspeed">
-            <div className="windspeed-value">{`${wind}km/hr`}</div>
-            <div className="windspeed-text">Wind Speed</div>
-          </div>
-        </div>
+        
       </div>
     </div>
   );
